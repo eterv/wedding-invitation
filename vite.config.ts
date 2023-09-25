b71360d488021b79c759480edfce7a53b767aaa2
Lucas Choi <eterv7@gmail.com>
2023-09-25 22:06:41 +0900

    chore: fixed vite, svelte, ts configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,19 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import tailwindcss from 'tailwindcss';
+import { imagetools } from 'vite-imagetools';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
-	}
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  plugins: [imagetools(), sveltekit()],
+  server: {
+    port: 2222,
+  },
+  test: {
+    include: ['src/**/*.{test,spec}.{js,ts}'],
+  },
 });
