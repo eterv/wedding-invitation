d98a3218b7293f69f67d0b1cc3de62e1ca05345b
Lucas Choi <eterv7@gmail.com>
2023-09-25 22:16:46 +0900

    Add +error, +layout

---
+++ src/routes/+error.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  const messages = {
+    404: 'Oops! Page Not Found :(',
+    500: 'Oops! Server Unavailable :(',
+  };
+</script>
+
+<section
+  class="fixed inset-0 flex h-screen w-screen items-center justify-center text-center"
+>
+  <div>
+    <h1 class="text-[7rem] font-bold">{$page.status}</h1>
+
+    {#if $page.status === 404}
+      <div class="text-32 font-light">{messages[$page.status]}</div>
+    {/if}
+  </div>
+</section>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { t } from '$lib/i18n';
+  import GNB from '~/components/nav/GNB.svelte';
+
+  import '~/styles/base.css';
+</script>
+
+<svelte:head>
+  <meta property="og:description" content={$t('og.desc')} />
+  <meta property="og:locale" content={$t('locale')} />
+  <meta property="og:site_name" content="GW & HI - Wedding Invitation" />
+  <meta property="og:title" content={$t('og.title')} />
+  <meta
+    property="og:image"
+    content="https://wedding.eterv.com/images/intro/intro.jpg"
+  />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://wedding.eterv.com" />
+
+  <title>{$t('title')}</title>
+</svelte:head>
+
+<header class="sticky left-0 right-0 top-0 z-10">
+  <GNB />
+</header>
+
+<main class="relative mx-auto max-w-800 bg-white">
+  <slot />
+</main>
+
+<footer class="bg-black3 pb-240 pt-48 text-center text-13 text-gray6">
+  <div>Made by eterv</div>
+  <div class="copyright">Copyright ⓒ 2020~ GW & HI, wedding.eterv.com</div>
+</footer>
