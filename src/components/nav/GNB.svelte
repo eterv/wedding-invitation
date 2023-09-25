fbddff4bd56654b68bcf3ce3534704c949f4ef73
Lucas Choi <eterv7@gmail.com>
2023-09-25 22:16:19 +0900

    Add GNB component

---
+++ src/components/nav/GNB.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  import GNBItem from './GNBItem.svelte';
+</script>
+
+<nav id="gnb" class="flex w-full border-b border-black4 bg-black3">
+  <GNBItem dest="top" text="HOME" />
+  <GNBItem dest="gallery" text="PHOTO" />
+  <GNBItem dest="location" text="MAP" />
+  <GNBItem dest="comment" text="MESSAGE" />
+</nav>
---
+++ src/components/nav/GNBItem.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { scrollToElement } from '../../utils';
+
+  export let dest: string;
+  export let text: string;
+
+  let el;
+
+  const onClick = (e: Event) => {
+    const target = e.target as HTMLAnchorElement;
+    const dest = target.dataset.dest;
+    scrollToElement('#' + dest);
+  };
+</script>
+
+<button
+  bind:this={el}
+  class="flex h-48 flex-1 items-center justify-center text-sm text-white"
+  data-dest={dest}
+  on:click={onClick}
+>
+  {text}
+</button>
