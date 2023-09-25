2019932e53195504b661ea0a84ddb3f59086696e
Lucas Choi <eterv7@gmail.com>
2023-09-25 22:13:11 +0900

    fix def types

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,12 +1,17 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-		// interface Error {}
-		// interface Locals {}
-		// interface PageData {}
-		// interface Platform {}
-	}
+  namespace App {
+    // interface Error {}
+    // interface Locals {}
+    // interface PageData {}
+    // interface Platform {}
+  }
+
+  interface Window {
+    Kakao: any;
+    //kakao: any;
+  }
 }
 
 export {};
