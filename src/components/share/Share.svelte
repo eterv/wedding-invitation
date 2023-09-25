0a760d33fcca81f8d021d4b5d951aca239b72d10
Lucas Choi <eterv7@gmail.com>
2023-09-25 22:22:41 +0900

    Add share component

---
+++ src/components/share/Share.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+  import Icon from '../icon/Icon.svelte';
+
+  import { page } from '$app/stores';
+  import { config } from '~/app.config';
+  import { t } from '~/lib/i18n';
+
+  const url = $page.url.origin;
+
+  const shareKakaotalk = () => {
+    window.Kakao.Link.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: $t('invitation.title'),
+        description: config.publicHost,
+        imageUrl: `${config.publicHost}/images/intro-text.jpg`,
+        link: {
+          mobileWebUrl: url,
+          webUrl: url,
+        },
+      },
+      buttons: [
+        {
+          title: $t('invitation.viewButton'),
+          link: {
+            mobileWebUrl: url,
+            webUrl: url,
+          },
+        },
+      ],
+    });
+  };
+</script>
+
+<div class="flex justify-center py-48">
+  <button
+    title="Sharing on KakaoTalk"
+    class="flex items-center justify-center"
+    on:click={shareKakaotalk}
+  >
+    <Icon name="kakaotalk" size={48} />
+  </button>
+</div>
