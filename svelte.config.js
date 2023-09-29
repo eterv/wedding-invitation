57b1d6f22d283f95910408d34ce1a3c62354c743
Lucas Choi <eterv7@gmail.com>
2023-09-29 15:59:44 +0900

    build: Change from Cloudflare to Vercel adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,5 @@
-import adapter from '@sveltejs/adapter-cloudflare';
+// import cloudflareAdapter from '@sveltejs/adapter-cloudflare';
+import vercelAdapter from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -15,11 +16,16 @@ const config = {
     // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
     // If your environment is not supported or you settled on a specific environment, switch out the adapter.
     // See https://kit.svelte.dev/docs/adapters for more information about adapters.
-    adapter: adapter({
-      routes: {
-        include: ['/*'],
-        exclude: ['<all>'],
-      },
+
+    // adapter: cloudflareAdapter({
+    //   routes: {
+    //     include: ['/*'],
+    //     exclude: ['<all>'],
+    //   },
+    // }),
+
+    adapter: vercelAdapter({
+      runtime: 'nodejs18.x',
     }),
   },
 };
